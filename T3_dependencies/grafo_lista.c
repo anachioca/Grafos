da4fce71f01f11e5b959c0f0f4c0590ad1af5e40
Ana Laura Chioca Vieira <ana.chioca@gmail.com>
2020-06-14 11:30:54 -0300

    I still need to recognize cycles in graphs

---
+++ T3_dependencies/grafo_lista.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "grafo_lista.h"
+#include "listaencad.h"
+
+
+GRAFO *criar_grafo(int numVertices){
+    GRAFO* A = (GRAFO*) malloc(sizeof(GRAFO)); 
+    if (A == NULL) return NULL;
+    A->lista = (LISTA**)malloc(numVertices*sizeof(LISTA*));
+
+    if (A->lista == NULL){
+        free(A);
+        return NULL;
+    }
+
+    for (int i = 0; i < numVertices; i++){
+        A->lista[i] = lista_criar();
+    }
+
+    A->numVertices = numVertices;
+    A->numArestas = 0;
+    return A;
+}
+
+GRAFO *freeGrafo(GRAFO *A){
+    if(A == NULL) return NULL;
+
+    for (int i = 0; i < A->numVertices; i++)
+        free(A->lista[i]);
+
+    free(A->lista);
+    free(A);
+    A = NULL;
+    return A;
+}
+
+int InsereAresta(GRAFO* A, int V1, int V2){  
+    if (A == NULL) return -1;
+    if (V1 < 0 || V1 > A->numVertices || V2 < 0 || V2 > A->numVertices) return -1;
+    if (lista_existe_no(A->lista[V1], V2) == 1) return -1;
+    
+    lista_inserir_fim(A->lista[V1], V2);
+    //if (V1 != V2) lista_inserir_fim(A->lista[V2], V1);
+
+    // Caso queira inserir os vértices de maneira ordenada:
+    // Lembrar do problema (não inserir desse modo se for o primeiro a ser inserido)
+    
+    //lista_inserir_ordenado(A->lista[V1], V2);
+    //if (V1 != V2) lista_inserir_ordenado(A->lista[V2], V1);
+    
+
+    A->numArestas++;
+    return 1;
+}
+
+int RetiraAresta(GRAFO* A, int V1, int V2){
+    if (A == NULL) return -1;
+    if (V1 < 0 || V1 > A->numVertices || V2 < 0 || V2 > A->numVertices) return -1;
+    
+    lista_remover(A->lista[V1], V2);
+    //lista_remover(A->lista[V2], V1);
+
+    A->numArestas--;
+    return 1;
+}
+
+int ImprimeGrafo(GRAFO *A){
+    if (A == NULL) 
+        printf("\nO grafo não existe.");
+    else{
+        for (int i = 0; i < A->numVertices; i++){
+            printf("%d: ", i);
+            lista_imprimir(A->lista[i]);
+        }
+    }
+    printf("\n");
+}
---
+++ T3_dependencies/main.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "listaencad.h"
+#include "grafo_lista.h"
+
+// verifica se todas as tarefas já foram executadas
+int finalizado(int *lista, int size){
+    for (int i = 0; i < size; i++){
+        if (lista[i] == 0) return 0;
+    }
+    return 1;
+}
+
+// deleta as arestas que representam dependências com vértices que já foram processados
+void atualizaGrafo(GRAFO *A, int *lista){
+    for (int i = 0; i < A->numVertices; i++){
+        if (lista[i] == 1){
+            for (int j = 0; j < A->numVertices; j++){
+                if (lista_existe_no(A->lista[j], i)) lista_remover(A->lista[j], i);
+            }
+        }
+    }
+}
+
+int GrafoPossuiCiclo(GRAFO *A){
+    // descobrir arestas de retorno..
+}
+
+int main(){
+
+    int m, n, V1, V2, count = 0;
+    scanf("%d", &n); //numero de vertices
+    scanf("%d", &m); //numero de arestas que serão inseridas
+    GRAFO *A = criar_grafo(n);
+    int * executado = (int*) calloc(n, sizeof(int));
+
+    for (int i = 0; i < m; i++){
+        scanf("%d %d", &V1, &V2);
+        InsereAresta(A, V2, V1); // preenche o grafo ao contrário!
+    }
+
+    if (GrafoPossuiCiclo(A)){
+        printf("%d", -1);
+        free(executado);
+        freeGrafo(A);
+        return 0;     
+    }
+
+    while (!finalizado(executado, n)){
+
+        // para cada atividade, caso sua lista de dependências esteja vazia, executá-la
+        for (int i = 0; i < n; i++){
+            
+            if (A->lista[i]->size == 0){
+                executado[i] = 1;
+            }
+        }
+        
+        atualizaGrafo(A, executado);
+        count++;
+    }
+
+    printf("%d", count);
+
+    free(executado);
+    freeGrafo(A);
+    return 0;    
+}
